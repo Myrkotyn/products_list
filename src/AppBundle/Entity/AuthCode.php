d713ad604c69da5822aba8bdf38550c9c9cd5d5d
Myrkotyn <prohorovychua@gmail.com>
2018-01-17 10:10:10 +0200

    auth code yml mapping

--- src/AppBundle/Entity/AuthCode.php
+++ src/AppBundle/Entity/AuthCode.php
@@ -1,31 +1,27 @@
 <?php
 
-
 namespace AppBundle\Entity;
 
 use FOS\OAuthServerBundle\Entity\AuthCode as BaseAuthCode;
-use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * Class AuthCode
+ * @package AppBundle\Entity
  */
 class AuthCode extends BaseAuthCode
 {
     /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
      */
     protected $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Client")
-     * @ORM\JoinColumn(nullable=false)
+     * @var Client
      */
     protected $client;
 
     /**
-     * @ORM\ManyToOne(targetEntity="User")
+     * @var User
      */
     protected $user;
 }
